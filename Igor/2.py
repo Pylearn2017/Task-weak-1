9525f88b3dcdb7afc9adf0ffc6fd55895bf0ebac
dragon26200387 <igor26200387@gmail.com>
2019-06-19 19:56:28 +0300

    Igor
    Igor

---
+++ Igor/2.py
@@ -0,0 +1,4 @@
+number = input()
+number = int(number)
+print("\n   _~_    \n  (o o)   \n /  V  \  \n/(  _  )\ \n  ^^ ^^" * number)
+input()
---
+++ Igor/3.py
@@ -0,0 +1,7 @@
+K = input("Enter K: ")
+K = int(K)
+N = input("Enter N: ")
+N = int(N)
+P = K//N
+print(P)
+input()
