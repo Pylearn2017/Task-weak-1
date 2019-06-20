c7d489ac068ea6bd47c91b47835e467f3b5b28bc
chelik <danya221107@gmail.com>
2019-06-20 18:23:01 +0300

    номер пять. попытка 2

--- 05_degree.py
+++ 05_degree.py
@@ -9,14 +9,12 @@
 Вывод программы:
 4
 '''
-print('введите число')
+
 num = input()
 num = int(num)
 
-print('в какую степень будет введенно число')
 step = input()
 step = int(step)
 
-print('ответ')
 print(num**step)
 input()
